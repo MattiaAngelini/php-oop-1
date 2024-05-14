e7541f4002f6ed44ee1ab2b9c72accbdc471467c
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-05-14 16:49:17 +0200

    feat: added class, instances, method and constructor and printed values ​​to screen.

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
-<?php 
+<?php
 
 class Movie {
     public $name;
     public $year;
     public $type;
+
+    public function __construct($_name, $_year, $_type) {
+        $this->name = $_name;
+        $this->year = $_year;
+        $this->type = $_type;
+    }
+
+    public function getInfoMovie() {
+        return $this->name . '' . $this->year. '' . $this->type;
+    }
+
 }
 
-?> 
+$matrix = new Movie("The Matrix", 1999, "Action");
+$gladiatore = new Movie("Gladiator", 2000, "Drama");
+
+
+echo $matrix->getInfoMovie();
+echo $gladiatore->getInfoMovie();
+
+
+?>
